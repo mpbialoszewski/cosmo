fcaddea2e5027139e90d8d8600bebb7b021a3760
mpbialoszewski <m.p.bialoszewski@outlook.com>
2025-08-17 16:59:49 +0100

    making react and ruby working together

---
+++ app/javascript/components/Start.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const Start = () => {
+  return <h1>Hello from React!</h1>;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('hello-react-app');
+  if (container) {
+    const root = createRoot(container);
+    root.render(<HelloComponent />);
+  }
+});
+
+export default Start;
